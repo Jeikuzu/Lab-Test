9c6c6ebbbc42f48c545affd45864c2bb560948be
Jeikuzu <duffyjake261@gmail.com>
2024-05-02 15:25:38 +0100

    e

--- Starter Code/exam.gd
+++ Starter Code/exam.gd
@@ -1,1 +1,12 @@
+extends Node2D
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	
+func _process(delta):
+	position = get_global_mouse_position()
+	
+	if event is InputEventMouseButton and event.pressed:
+		$Explosion
+	
 
